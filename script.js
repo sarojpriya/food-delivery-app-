c285f990a8a042e8185178346802a5546a9df22c
saroj priya <sarojpriya0803@gmail.com>
2026-05-16 12:19:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        alert("Item Added Successfully!");
+
+    });
+
+});
